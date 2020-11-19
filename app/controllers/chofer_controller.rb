4fb6fdbf888c844611a1306a6bdcfb5e6fd9afc5
Monto <montoya.m97@hotmail.com>
2020-11-19 19:00:42 -0300

    Chofer act

--- app/controllers/chofer_controller.rb
+++ app/controllers/chofer_controller.rb
@@ -1,21 +1,23 @@
 class ChoferController < ApplicationController
-
+	before_action :authenticate_user! 
 	def index
 		@choferes= User.where(role: "chofer")
 	end
 	def show
+	 @chofer= User.find(params[:id])
 
 	end
 	def edit
-	 @chofer= User.find(params[:id])
+		@chofer= User.find(params[:id]) 
+	 
 	end
 
 	def new
 		@chofer= User.new
 	end
 	def create
-	@chofer= User.new(chofer_params)
-	@chofer.role= "chofer"
+     @chofer= User.new(chofer_params)
+	 @chofer.role= "chofer"
 	if @chofer.save!
 		redirect_to chofer_index_path, notice: "Registro exitoso"
 	else
@@ -25,17 +27,17 @@ class ChoferController < ApplicationController
 	def update
 		@chofer = User.find(params[:id])
 		if @chofer.update(chofer_params)
-			redirect_to chofer_path
+			redirect_to chofer_index_path, notice: "Datos actualizados"
 		else 
-			render 'edit'
+			render :edit
 		end
 	end	
 	def destroy
 		@chofer= User.find(params[:id])
 		@chofer.destroy
-		redirect_to chofer_path
-
+		redirect_to chofer_index_path
 	end
+	
 	private
 	def chofer_params
 		params.require(:user).permit([:email,:password,:password_confirmation,:nombre,:apellido,:fecha_de_nacimiento,:dni,:celular])
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,17 @@ Rails.application.routes.draw do
 
   root to: "home#index"
 
-  resources :chofer  
+  resources :chofer
+  get '/chofer/index', to: 'chofer#index'
+  get '/chofer/new', to: 'chofer#new'
+  post '/chofer' , to: 'chofer#create'
+  get '/chofer/:id', to: 'chofer#show'
+  get '/chofer/:id/edit', to: 'chofer#edit'
+  put '/chofer/:id', to: 'chofer#update'
+  patch '/chofer/:id', to: 'chofer#update'
+  delete '/chofer/destroy/:id', to: 'chofer#destroy', as: 'destroy_chofer'
+  get '/chofer/show/:id', to: 'chofer#show' , as: 'show_chofer'
+  
  
  
   end
